b0cb7ca188d0bacbc257e2c36042910b661a8349
rahmah2009 <muhammadrahmah67@gmail.com>
2026-08-27 15:24:01 +0100

    Implement category-based spending breakdown

---
+++ reports.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+transactions = [
+    {
+        "type": "expense",
+        "category": "Food",
+        "amount": 5000
+    },
+    {
+        "type": "expense",
+        "category": "Transport",
+        "amount": 3000
+    },
+    {
+        "type": "expense",
+        "category": "Food",
+        "amount": 2000
+    },
+    {
+        "type": "expense",
+        "category": "Data",
+        "amount": 1500
+    }
+]
+
+def view_by_category():
+    categories = {}
+
+    for transaction in transactions:
+        if transaction["type"] == "expense":
+            category = transaction['category']
+            amount = transaction['amount']
+
+        if category not in categories:
+            categories[category] = 0
+
+        categories[category] += amount
+
+    print("\n===== SPENDING BY CATEGORY =====")
+
+    if not categories:
+        print("No expenses found.")
+        return
+
+    for category, total in categories.items():
+        print(f"{category}: ₦{total:,.2f}")
+
+view_by_category()
